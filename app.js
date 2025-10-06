9fb33c60098f86852333f405bddfbd0f0bfb08d2
Mounika77754 <mouni77754@gmail.com>
2025-10-06 21:17:34 +0530

    done

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ mongoose.connect(`${process.env.MONGO_URI}`,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>console.log("connected")).catch((err)=>{console.log(err.message)});
+app.get("/", (req, res) => {
+  res.send("✅ Backend is live and running!");
+});
+
 app.post("/register",async(req,res)=>{
     const {userName,userEmail,Password}=req.body;
    try{
